7a34520600657cd08763766fdff7f5ebcc3656e2
ogabek0909 <norpulatovogabek42@gmail.com>
2024-03-17 12:59:52 +0500

    changing architecture

--- lib/config/router/routes.dart
+++ lib/config/router/routes.dart
@@ -1,13 +1,6 @@
 import 'package:go_router/go_router.dart';
 
-import '../../presentation/views/home_screen.dart';
-
 GoRouter appRoutes = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.routeName,
-      builder: (context, state) =>const HomeScreen(),
-    ),
   ],
 );
---
+++ lib/presentation/view/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const routeName = 'home-screen';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
